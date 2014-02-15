83a69a5cea9f3de69627c7e7021c7f3a463a6263
Chintan Parikh <chintanparikh@gatech.edu>
2014-02-15 12:57:54 -0500

    asdf

--- bitvendr.rb
+++ bitvendr.rb
@@ -17,31 +17,35 @@ class Bitvendr < Sinatra::Base
   	# If it fails, then send the money back to the user and text the user saying it failed and the money has been returned
   	# If it succeeds, text the user saying it succeeded and its in the works + email address if something messes up.
 
- 
-  	params = JSON.parse(request.body.read)
-  	note = params["data"]["note"]
-  	id = params["data"]["actor"]["id"]
-  	payment_id = params["data"]["id"]
-  	amount = Sinatra::BitvendrHelpers::Venmo.get_amount(payment_id).abs
+ 	begin
+	  	params = JSON.parse(request.body.read)
+	  	note = params["data"]["note"]
+	  	id = params["data"]["actor"]["id"]
+	  	payment_id = params["data"]["id"]
+	  	amount = Sinatra::BitvendrHelpers::Venmo.get_amount(payment_id).abs
 
-  	if params["data"]["action"] == "pay"
-  		coinbase = Sinatra::BitvendrHelpers::Payment.auth
+	  	if params["data"]["action"] == "pay"
+	  		coinbase = Sinatra::BitvendrHelpers::Payment.auth
 
-  		btc = amount / Sinatra::BitvendrHelpers::Payment.price
-  		if coinbase.balance.to_f > btc
-  			btc_to_send = btc * 0.90 #fucktheuser
-  			response = coinbase.send_money note, btc_to_send
+	  		btc = amount / Sinatra::BitvendrHelpers::Payment.price
+	  		if coinbase.balance.to_f > btc
+	  			btc_to_send = btc * 0.90 #fucktheuser
+	  			response = coinbase.send_money note, btc_to_send
 
-  			if response.success?
-  				success btc_to_send, id
-  				# coinbase.buy!(btc * 0.95)
-  			else
-  				error btc_to_send, id
-  			end
-  		else
-  			error btc_to_send, id
-  		end
-  	end
+	  			if response.success?
+	  				success btc_to_send, id
+	  				# coinbase.buy!(btc * 0.95)
+	  			else
+	  				error btc_to_send, id
+	  			end
+	  		else
+	  			error btc_to_send, id
+	  		end
+	  	end
+	  rescue Exception => msg
+	  	puts msg
+	  	200
+	  end
   end
 
   get '/process_payment' do
